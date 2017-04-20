83d00d160362a36b1f3984330f32ae2dda555a83
Venkat <chaitra.infanion@gmail.com>
2017-04-20 17:03:07 +0530

    Script 1 updated

---
+++ MavenChaitra/src/test/java/MavenChaitra/testscripts/Script1.java
@@ -0,0 +1,28 @@
+package MavenChaitra.testscripts;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import MavenChaitra.MavenChaitra.Driver;
+import MavenChaitra.MavenChaitra.WebDriverCommonlib;
+import MavenChaitra.PageObjectRepo.PageRepo;
+
+public class Script1 extends WebDriverCommonlib{
+	
+	WebDriver driver;
+	PageRepo homepageloading;
+	
+	
+	@Test
+	public void openbrowser() throws IOException
+	{
+		driver=Driver.getDriver();
+		System.out.println("Hi the driver URL is "  +driver);
+		homepageloading=PageFactory.initElements(driver, PageRepo.class);
+		homepageloading.hitUrl();
+		
+	}	
+}
